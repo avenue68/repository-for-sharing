1939f1e6ebda2fcd2401fcafd4f3f5c83acb99f9
Jinya Namiki <83228978+avenue68@users.noreply.github.com>
2022-01-02 20:10:58 +0900

    削除

--- stackoverflow-70491554/referencing-module/src/main/java/com/example/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class Main {
-
-	public static void main(String[] args) {
-		var concreteClass = new MyConcreteClass();
-		System.out.println(concreteClass.value());
-	}
-}
--- stackoverflow-70491554/referencing-module/src/main/java/com/example/MyConcreteClass.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-import com.example.referenced.MyAbstractClass;
-
-public class MyConcreteClass extends MyAbstractClass {
-
-}
